35332042bf089719cb4618eb6bb2353a19b1e695
Andrew <32229265+aselee@users.noreply.github.com>
2018-10-15 01:13:36 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Titles from './components/Titles'
+import Form from './components/Form'
+import Weather from './components/Weather'
 
-class App extends Component {
+
+const API_KEY="16e9cba97fb8af6f31f4a41263633ce8";
+
+
+class WeatherApp extends React.Component {
+
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined,
+
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    
+    const data = await api_call.json();
+
+    // If city equals true AND country equals true,
+    // render the code, if not, do nothing.
+    // if (city === true && country === true) {}
+
+    if ( city && country ) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+      // else if no value has been added, error message will show
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter city and country."
+      });
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export default WeatherApp;
